9d4ccc750439806bed7de8a72ea5c7c0bbee8a11
Jared Sy <jaredblase@gmail.com>
2025-07-23 00:22:26 +0800

    refactor: format code

--- src/fonts/outfit.tsx
+++ src/fonts/outfit.tsx
@@ -1,8 +1,8 @@
-import { Outfit } from 'next/font/google';
+import { Outfit } from 'next/font/google'
 
 export const outfit = Outfit({
 	subsets: ['latin'],
 	variable: '--font-tertiary',
 	display: 'swap',
 	weight: ['400'],
-});
+})
