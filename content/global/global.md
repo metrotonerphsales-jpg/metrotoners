0c7135c2448ac34cb665ad224d471b4d271559a6
Jared Sy <jaredblase@gmail.com>
2025-07-23 00:58:42 +0800

    add: tina

---
+++ content/global/global.md
@@ -0,0 +1,13 @@
+---
+addresses:
+  - name: Manila Branch
+    address: >-
+      3F Grayline Ventures Bldg. 14 Arayat St., Brgy. Malamig, Mandaluyong,
+      Philippines
+    tel: 8-723-4894/8-725-7031
+  - name: Cebu Branch
+    address: '#60 Aya-ay Arcade, Mango Ave., Cebu City, Philippines'
+    tel: 032-2606941
+
+email: metrotoners@gmail.com
+---
---
+++ tina/config.ts
@@ -0,0 +1,80 @@
+import { defineConfig } from 'tinacms'
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+	process.env.GITHUB_BRANCH ||
+	process.env.VERCEL_GIT_COMMIT_REF ||
+	process.env.HEAD ||
+	'main'
+
+export default defineConfig({
+	branch,
+	clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+	token: process.env.TINA_TOKEN,
+
+	build: {
+		outputFolder: 'admin',
+		publicFolder: 'public',
+	},
+	media: {
+		tina: {
+			mediaRoot: '',
+			publicFolder: 'public',
+		},
+	},
+	schema: {
+		collections: [
+			{
+				name: 'Global',
+				label: 'Global',
+				path: 'content/global',
+				fields: [
+					{
+						type: 'string',
+						name: 'email',
+						label: 'Email',
+						required: true,
+					},
+					{
+						type: 'object',
+						name: 'addresses',
+						label: 'Addresses',
+						list: true,
+						fields: [
+							{
+								type: 'string',
+								name: 'name',
+								label: 'Name',
+								required: true,
+							},
+							{
+								type: 'string',
+								name: 'address',
+								label: 'Address',
+								required: true,
+							},
+							{
+								type: 'string',
+								name: 'tel',
+								label: 'Telephone Number(s)',
+								required: true,
+							},
+						],
+						required: true,
+						ui: {
+							itemProps(props) {
+								return { label: props.name }
+							},
+						},
+					},
+				],
+				ui: {
+					allowedActions: {
+						create: false,
+						delete: false,
+					},
+				},
+			},
+		],
+	},
+})
