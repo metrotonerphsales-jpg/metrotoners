1f3493d125d495e32c2639a8679b64a6273a3aeb
Jared Sy <jaredblase@gmail.com>
2025-08-10 19:12:06 +0800

    update: pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import { ProductBlock } from "@/components/product-block";
+
 export default function Home() {
 	return (
 		<div className="container mx-auto px-4 py-8">
-			<h1 className="text-4xl font-bold mb-6 font-primary">Toners</h1>
-			<p className="text-lg text-gray-700">
-				Explore our wide range of products and services designed to enhance your
-				life in the city.
-			</p>
-			{/* Add more content here as needed */}
+			<div className="relative flex justify-center">
+				<hr className="border-primary border-[1.5px] absolute top-1/2 left-0 w-full" />
+				<h1 className="text-4xl font-bold font-primary bg-white relative px-6">Toners</h1>
+			</div>
+			<div className="grid grid-cols-4">
+				<ProductBlock name="DR-261CL" make="Brother" price={4900}></ProductBlock>
+			</div>
 		</div>
 	)
 }
