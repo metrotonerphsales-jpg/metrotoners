293e16f5352accdc643a9b883ce84167305dc880
Jared Sy <jaredblase@gmail.com>
2025-08-16 16:11:33 +0800

    update: product block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,13 @@ import { ProductBlock } from '@/components/product-block'
 export default function Home() {
 	return (
 		<div className="container mx-auto px-4 py-8">
-			<div className="relative flex justify-center">
+			<div className="relative mb-4 flex justify-center">
 				<hr className="border-primary absolute top-1/2 left-0 w-full border-[1.5px]" />
-				<h1 className="font-primary relative bg-white px-6 text-4xl font-bold">
+				<h1 className="font-primary text-primary relative bg-white px-6 text-3xl font-bold sm:text-4xl">
 					Toners
 				</h1>
 			</div>
-			<div className="grid grid-cols-4">
-				<ProductBlock
-					name="DR-261CL"
-					make="Brother"
-					price={4900}
-				></ProductBlock>
-			</div>
+			<div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"></div>
 		</div>
 	)
 }
--- src/components/product-block/product-block.tsx
+++ src/components/product-block/product-block.tsx
@@ -1,31 +1,47 @@
 import Image from 'next/image'
 import { Button } from '../ui/button'
-import logo from '@/app/icon.svg'
 import { formatPrice } from '@/lib/utils'
+import Link from 'next/link'
 
-type Product = {
-	name: string
-	make: string
+type ProductBlockProps = {
+	title: string
+	subtitle: string
 	price: number
+	src: string
+	alt: string
+	url: string
 }
 
-export function ProductBlock({ name, make, price }: Product) {
+export function ProductBlock({
+	title,
+	subtitle,
+	price,
+	src,
+	alt,
+	url,
+}: ProductBlockProps) {
 	return (
-		<div className="text-primary border-primary border">
-			<Image src={logo} alt="printer" />
+		<Link className="text-primary border-primary block border" href={url}>
+			<Image width={362} height={305} src={src} alt={alt} />
 			<div>
 				<p className="font-primary mt-2 text-center text-3xl font-bold">
-					{name}
+					{title}
 				</p>
-				<p className="font-secondary text-center">{make}</p>
-				<p className="font-secondary mt-2 text-center text-xl">
-					{formatPrice(price)}
-				</p>
-				<div className="mt-4 flex justify-center gap-x-2">
-					<Button variant="outline">Learn More</Button>
-					<Button>Inquire Now</Button>
+				<div className="pb-4">
+					<p className="font-secondary -mt-1 text-center">{subtitle}</p>
+					<p className="font-secondary mt-2 text-center text-xl">
+						{formatPrice(price)}
+					</p>
+					<div className="mt-4 flex flex-wrap justify-center gap-2">
+						<Button variant="outline" className="text-lg">
+							Learn More
+						</Button>
+						<Button variant="secondary" className="text-lg">
+							Inquire Now
+						</Button>
+					</div>
 				</div>
 			</div>
-		</div>
+		</Link>
 	)
 }
