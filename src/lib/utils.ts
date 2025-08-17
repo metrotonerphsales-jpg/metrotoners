3e9e35c2e18a9d222f6f764f6609f4ae52cdcd62
Jared Sy <jaredblase@gmail.com>
2025-08-17 20:33:38 +0800

    add: filter utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,33 @@ export function formatPrice(p: number) {
 export function filterFalsy<T>(item?: T | null): item is T {
 	return !!item
 }
+
+export function createFilterValues(
+	attributes: string[],
+	products: Array<Record<string, any>>
+) {
+	return attributes.reduce((filterValues, attribute) => {
+		const uniqueValues = products.reduce((prev, item) => {
+			if (item[attribute]) prev.add(item[attribute])
+			return prev
+		}, new Set()) as Set<string>
+
+		if (uniqueValues.size) {
+			filterValues[attribute] = uniqueValues
+		}
+
+		return filterValues
+	}, {} as FilterValues)
+}
+
+export function createCustomFilter(selection: SelectionValues) {
+	return (object: Record<string, any>) => {
+		for (const key in selection) {
+			if (selection[key]?.size && !selection[key].has(object[key])) {
+				return false
+			}
+		}
+
+		return true
+	}
+}
