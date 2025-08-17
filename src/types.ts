9b5d52108457d09b2f4c4bf436416ed544275d19
Jared Sy <jaredblase@gmail.com>
2025-08-17 22:02:08 +0800

    update: global types

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,5 @@
 export type FilterValues = Record<string, Set<string>>
 export type SelectionValues = Record<string, Set<string> | undefined>
+export type NextJSPageProps = {
+	searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
